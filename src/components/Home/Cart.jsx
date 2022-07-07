e90a02e8fe22f8ebe2646e185479290b9d06a0f4
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-07-06 22:29:35 -0500

    Intento de logica de carrito

--- src/components/Home/Cart.jsx
+++ src/components/Home/Cart.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react'
 import ItemCart from './ItemCart'
 import axios from 'axios'
 import getConfig from '../../utils/getConfig.js'
+import { useDispatch, useSelector } from 'react-redux'
+import { setCartProducts } from '../../store/slices/cartProducts.slice.js'
 
 const Cart = ({cartOpen}) => {
-  const [products, setProducts] = useState([])
-
+  const cartProducts = useSelector(state => state.cartProducts)
+  const dispatch = useDispatch()
+  
   useEffect(() => {
     const url = 'https://ecommerce-api-react.herokuapp.com/api/v1/cart'
     axios.get(url, getConfig())
-    .then(res => setProducts(res.data.data.cart.products))
+    .then(res => dispatch(setCartProducts(res.data.data.cart.products)))
     .catch(err => console.log(err))
   }, [])
 
@@ -18,7 +21,7 @@ const Cart = ({cartOpen}) => {
         <h2 className='cart__title'>Shopping Cart</h2>
         <section className='cart__products'>
           {
-            products.map(product => <ItemCart key={product.id} product={product}/>)
+            cartProducts.map(product => <ItemCart key={product.id} product={product}/>)
           }
         </section>
         <section className='cart__results'>
